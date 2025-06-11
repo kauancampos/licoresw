2187ba10f15a8b83ccc7d455fd652fcbb1e6bead
kauancampos <kauanpablofreitascampos@gmail.com>
2025-06-11 16:43:12 -0300

    Adicionando emoji do carrinho
    Adc carrinho

---
+++ src/app/components/ShoopingCart.tsx
@@ -0,0 +1,8 @@
+import { ShoppingCart as ShoppingCartIcon } from "lucide-react";
+
+function ShoppingCart() {
+  return <ShoppingCartIcon size={20} color="#fff" />;
+    
+}
+
+export default ShoppingCart;
--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Image from 'next/image';
 import style from '../styles/Navbar.module.css'; 
 import logo from '../imgs/logo.png'
-
+import ShoppingCart from './ShoopingCart'
 
 
 function Navbar() {
@@ -12,7 +12,7 @@ function Navbar() {
                 <Image className={style.logo} src={logo} alt="Logo da Wlicores" width={400} height={350} />
                 <ul className={style.opcoes}>
                     <li className={style.opcao}><a href="/page">Home</a></li>
-                    <li className={style.opcao}><a href="/produtos">Produtos</a></li>
+                    <li className={style.opcao}><ShoppingCart /><a href="/produtos">Produtos</a></li>
                     <li className={style.opcao}><a href="/sobre">Sobre</a></li>
                     <li className={style.opcao}><a href="/contato">Contato</a></li>
                     <button className={style.cadastro}><a href="/cadastro">Cadastro</a></button>
